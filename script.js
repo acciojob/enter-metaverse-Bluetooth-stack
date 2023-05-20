e68fc0cbea5500f273944cfa50bbe35a8ed7c788
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-05-21 00:44:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
 document.querySelector('#enterBtn').addEventListener('click',()=>{
-	document.querySelector('#status').innerText = 'Entered Metaverse';
+	document.querySelector('#status').innerHTML = '<h1>Entered Metaverse</h1>';
 })
